ad25d6e383e1b8f7805c818843ac2bd6cf3cf19f
jakerdou <hem.adtigo@gmail.com>
2021-12-05 21:25:38 +0000

    changes

--- LED.py
+++ LED.py
@@ -3,24 +3,27 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-red = 16
-green = 26
+red = 26
+green = 16
 
 
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 
 def yellow_light():   #calibration
-    GPIO.output(red, 1)
-    GPIO.output(green, 1)
+    GPIO.output(red, 0)
+    GPIO.output(green, 0)
 
 def green_light():    #in camera view
-    GPIO.output(green, 1)
-    GPIO.output(red, 0)
-
-def red_light():      #not in camera view
     GPIO.output(red, 1)
     GPIO.output(green, 0)
 
-def off(pin):
-    GPIO.output(pin, 0)
+def red_light():      #not in camera view
+    GPIO.output(red, 0)
+    GPIO.output(green, 1)
+
+def off():
+    GPIO.output(red, 1)
+    GPIO.output(green, 1)
+    
+off()
